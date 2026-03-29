229ab859505c93f5a3aa5cd68da36a2eba44bd55
Liekoo <veyeka1@gmail.com>
2026-03-29 09:40:00 +0800

    sm6225-common: patch usb blobs to use QTI health AIDL instead

--- extract-files.py
+++ extract-files.py
@@ -87,6 +87,15 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-fromvendor.so'),
     'system_ext/lib64/libprotobuf-cpp-full-fromvendor.so': blob_fixup()
         .replace_needed('libprotobuf-cpp-lite.so','libprotobuf-cpp-lite-fromvendor.so'),
+    
+    'vendor/etc/init/init.batterysecret.rc': blob_fixup()
+    .regex_replace('on charger', 'on property:init.svc.vendor.charger=running'),
+
+    'vendor/etc/init/hw/init.qcom.usb.rc': blob_fixup()
+    .regex_replace('on charger', 'on property:init.svc.vendor.charger=running'),
+
+     'vendor/etc/init/init.mi_thermald.rc': blob_fixup()
+    .regex_replace('on charger', 'on property:init.svc.vendor.charger=running'),
 
 }
 
