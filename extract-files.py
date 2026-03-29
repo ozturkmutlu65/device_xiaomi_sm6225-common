7b63c3da4f7dcbc77afbdd386660e376c079ee96
Masood-J <m.masood7679@gmail.com>
2026-03-29 09:18:45 +0800

    sm6225-common: import required dependency for vendor.qti.hardware.qccsyshal@1.2-halimpl.so
    * Fixes the following spam log:
     08-21 21:22:07.492 F linker  : CANNOT LINK EXECUTABLE
    "/system/system_ext/bin/qccsyshal@1.2-service": cannot locate symbol
    "_ZN6google8protobuf7Message19CopyWithSourceCheckERS1_RKS1_"

--- extract-files.py
+++ extract-files.py
@@ -83,6 +83,11 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/libdpps.so': blob_fixup()
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
 
+    'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
+        .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-fromvendor.so'),
+    'system_ext/lib64/libprotobuf-cpp-full-fromvendor.so': blob_fixup()
+        .replace_needed('libprotobuf-cpp-lite.so','libprotobuf-cpp-lite-fromvendor.so'),
+
 }
 
 module = ExtractUtilsModule(
